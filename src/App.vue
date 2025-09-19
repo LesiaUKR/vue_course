0350b0b8ebfc86fccb32d258cb119effb9c9f429
Lesia Soloviova <solovyova.lesya@gmail.com>
2025-09-19 21:49:43 +0100

    discovered v-bind and v-for directives

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-
 <template>
-<MainNav />
+  <MainNav />
+  <div
+    class="rounded-lg bg-red-500 p-4 text-white shadow-md transition-colors duration-200 hover:bg-red-600"
+  >
+    Test
+  </div>
 </template>
 
 <script setup>
-import MainNav from "@/components/MainNav.vue"
+import MainNav from '@/components/MainNav.vue'
 </script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,13 +1,41 @@
 <template>
   <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 w-full h-16 bg-white">
+    <div class="fixed left-0 top-0 h-16 w-full bg-white">
       <div
-        class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto"
+        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
       >
-        <a class="flex h-full items-center text-xl" href="/">Bobo Careers</a>
+        <a :href="companyUrl" class="flex h-full items-center text-xl">{{
+          company
+        }}</a>
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li
+              v-for="menuItem in menuItems"
+              :key="menuItem"
+              class="ml-9 h-full first:ml-0"
+            >
+              <a href="" class="flex h-full items-center py-2.5">{{
+                menuItem
+              }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const company = ref('Bobo Careers')
+const companyUrl = ref('https://bobocareers.com')
+const menuItems = [
+  'Teams',
+  'Location',
+  'Life at Bobo Corp',
+  'How we hire',
+  'Students',
+  'Jobs',
+]
+</script>
